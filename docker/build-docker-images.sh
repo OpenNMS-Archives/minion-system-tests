c4d90ae9b943c6a389c38c30e251f1c310f49738
Jesse White <jesse@opennms.org>
2015-03-20 11:42:04 -0400

    Fail immediately if building/pulling any of the images fail.

--- docker/build-docker-images.sh
+++ docker/build-docker-images.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+[ ! -d dominion ] && echo "The script must be invoked from the docker subdirectory of the project." && exit 1
 
 POSTGRES=$(docker images | grep postgres | awk '{print $1}')
 if [[ -z $POSTGRES ]]; then
   echo "Pulling postgres image from public registry"
-  docker pull postgres
+  docker pull postgres || exit 1
 fi
 
 echo "Builing Minion image"
-docker build -t minion:v1 ./minion
+docker build -t minion:v1 ./minion || exit 1
 
 echo "Builing Dominion image"
-docker build -t dominion:v1 ./dominion
+docker build -t dominion:v1 ./dominion || exit 1
 
 echo "Building snmpd image"
-docker build -t snmpd:v1 ./snmpd
+docker build -t snmpd:v1 ./snmpd || exit 1

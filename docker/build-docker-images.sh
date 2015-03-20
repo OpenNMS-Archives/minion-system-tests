4245ec7ea4013be75225fe668af20103ab887d2d
Jesse White <jesse@opennms.org>
2015-03-20 11:30:24 -0400

    Pull the postgres image from the registry if it's not already installed.

--- docker/build-docker-images.sh
+++ docker/build-docker-images.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+POSTGRES=$(docker images | grep postgres | awk '{print $1}')
+if [[ -z $POSTGRES ]]; then
+  echo "Pulling postgres image from public registry"
+  docker pull postgres
+fi
+
+echo "Builing Minion image"
 docker build -t minion:v1 ./minion
+
+echo "Builing Dominion image"
 docker build -t dominion:v1 ./dominion
+
+echo "Building snmpd image"
 docker build -t snmpd:v1 ./snmpd

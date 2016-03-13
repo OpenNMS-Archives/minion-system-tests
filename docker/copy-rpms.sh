f198e8bdd16bd9fb70139c52e9a00b9f4022e4c7
Jesse White <jesse@opennms.org>
2016-03-12 23:47:44 -0500

    Don't rely on OpenNMS's pom.xml to determine the version of the RPMs.

--- docker/copy-rpms.sh
+++ docker/copy-rpms.sh
@@ -1,25 +1,25 @@
-#!/bin/bash -e
-if [ -z $OPENNMS_BUILD ]; then
-  OPENNMS_BUILD="$HOME/git/opennms"
+#!/bin/sh -e
+if [ -z $OPENNMS_RPM_ROOT ]; then
+  if [ -z $OPENNMS_BUILD ]; then
+    OPENNMS_BUILD="$HOME/git/opennms"
+  fi
+  OPENNMS_RPM_ROOT="$OPENNMS_BUILD/target/rpm/RPMS/noarch"
 fi
 
-RPM_ROOT="$OPENNMS_BUILD/target/rpm/RPMS/noarch"
-test -e $RPM_ROOT || (echo "Cannot find RPMs in '$RPM_ROOT'. Try running ./makerpm.sh from '$OPENNMS_BUILD'." && exit 1)
+test -e $OPENNMS_RPM_ROOT || (echo "Cannot find RPMs in '$OPENNMS_RPM_ROOT'." && exit 1)
 
-pushd $OPENNMS_BUILD
-RELEASE=$(python -c "import xml.etree.ElementTree as ET; print(ET.parse(open('pom.xml')).getroot().find( '{http://maven.apache.org/POM/4.0.0}version').text)";)
-RELEASE=$(echo $RELEASE | awk -F '-' '{ print $1; }')
-popd
+# Grab the release number from one of the known RPMs so we can filter the RPMs we need
+export RELEASE=$(basename $OPENNMS_RPM_ROOT/opennms-minion-features-core-*.noarch.rpm | awk -F'-' '{ print $5; }')
 
 mkdir -p opennms/rpms
 rm -rf opennms/rpms/*.rpm
-cp $RPM_ROOT/opennms-webapp-jetty-$RELEASE*.noarch.rpm opennms/rpms/opennms-webapp-jetty.noarch.rpm
-cp $RPM_ROOT/opennms-core-$RELEASE*.noarch.rpm opennms/rpms/opennms-core.noarch.rpm
-cp $RPM_ROOT/opennms-$RELEASE*.noarch.rpm opennms/rpms/opennms.noarch.rpm
+cp $OPENNMS_RPM_ROOT/opennms-webapp-jetty-$RELEASE*.noarch.rpm opennms/rpms/opennms-webapp-jetty.noarch.rpm
+cp $OPENNMS_RPM_ROOT/opennms-core-$RELEASE*.noarch.rpm opennms/rpms/opennms-core.noarch.rpm
+cp $OPENNMS_RPM_ROOT/opennms-$RELEASE*.noarch.rpm opennms/rpms/opennms.noarch.rpm
 
 mkdir -p minion/rpms
 rm -rf minion/rpms/*.rpm
-cp $RPM_ROOT/opennms-minion-$RELEASE*.noarch.rpm  minion/rpms/opennms-minion.noarch.rpm
-cp $RPM_ROOT/opennms-minion-container-$RELEASE*.noarch.rpm minion/rpms/opennms-minion-container.noarch.rpm
-cp $RPM_ROOT/opennms-minion-features-core-$RELEASE*.noarch.rpm minion/rpms/opennms-minion-features-core.noarch.rpm
-cp $RPM_ROOT/opennms-minion-features-default-$RELEASE*.noarch.rpm minion/rpms/opennms-minion-features-default.noarch.rpm
+cp $OPENNMS_RPM_ROOT/opennms-minion-$RELEASE*.noarch.rpm  minion/rpms/opennms-minion.noarch.rpm
+cp $OPENNMS_RPM_ROOT/opennms-minion-container-$RELEASE*.noarch.rpm minion/rpms/opennms-minion-container.noarch.rpm
+cp $OPENNMS_RPM_ROOT/opennms-minion-features-core-$RELEASE*.noarch.rpm minion/rpms/opennms-minion-features-core.noarch.rpm
+cp $OPENNMS_RPM_ROOT/opennms-minion-features-default-$RELEASE*.noarch.rpm minion/rpms/opennms-minion-features-default.noarch.rpm

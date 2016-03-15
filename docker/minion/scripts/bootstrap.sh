346b526e9fecde3bde84e1b2ec6b104a3e31b625
Jesse White <jesse@opennms.org>
2016-03-15 10:39:42 -0700

    Expose the Karaf Shell and RMI ports now that they are bound to 127.0.0.1 by default.

--- docker/minion/scripts/bootstrap.sh
+++ docker/minion/scripts/bootstrap.sh
@@ -3,11 +3,17 @@ MINION_HOME=/opt/minion
 
 echo "MINION HOME: ${MINION_HOME}"
 
+# Expose the Karaf shell
+sed -i s/sshHost.*/sshHost=0.0.0.0/g "${MINION_HOME}/etc/org.apache.karaf.shell.cfg"
+
+# Expose the RMI registry and server
+sed -i s/rmiRegistryHost.*/rmiRegistryHost=0.0.0.0/g "${MINION_HOME}/etc/org.apache.karaf.management.cfg"
+sed -i s/rmiServerHost.*/rmiServerHost=0.0.0.0/g "${MINION_HOME}/etc/org.apache.karaf.management.cfg"
+
 echo "location = MINION" > $MINION_HOME/etc/org.opennms.minion.controller.cfg
 echo "id = 00000000-0000-0000-0000-000000ddba11" >> $MINION_HOME/etc/org.opennms.minion.controller.cfg
+echo "http-url = http://${OPENNMS_PORT_8980_TCP_ADDR}:${OPENNMS_PORT_8980_TCP_PORT}" >> $MINION_HOME/etc/org.opennms.minion.controller.cfg
 echo "broker-url = tcp://${OPENNMS_PORT_61616_TCP_ADDR}:${OPENNMS_PORT_61616_TCP_PORT}" >> $MINION_HOME/etc/org.opennms.minion.controller.cfg
-echo "username = admin" >> $MINION_HOME/etc/org.opennms.minion.controller.cfg
-echo "password = admin" >> $MINION_HOME/etc/org.opennms.minion.controller.cfg
 
 rm -rf $MINION_HOME/data
 $MINION_HOME/bin/karaf clean server
